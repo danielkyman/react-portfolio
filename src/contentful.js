f6915decfeeed9e1b7ab8e755526e51a2f0e48cd
danielkyman <danny.kyman@gmail.com>
2020-03-29 22:10:04 -0700

    transfered api keys to own file

---
+++ src/contentful.js
@@ -0,0 +1,3 @@
+export const API_BASE_URL = "https://cdn.contentful.com";
+export const API_SPACE_ID = "q2fivpqovvam";
+export const API_TOKEN = "aq9YgRIXsgEKRLXG8bAC2mM5wChSJddYq2dSbvpS89k";
--- src/redux/actions/blogPost.js
+++ src/redux/actions/blogPost.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-export const GET_BLOGPOST_START = "GET_BLOGPOST_START";
-export const GET_BLOGPOST_SUCCESS = "GET_BLOGPOST_SUCCESS";
-export const GET_BLOGPOST_ERROR = "GET_BLOGPOST_ERROR";
+import { API_BASE_URL, API_SPACE_ID, API_TOKEN } from "../../contentful";
 
-const API_BASE_URL = "https://cdn.contentful.com";
-const API_SPACE_ID = "q2fivpqovvam";
-const API_TOKEN = "aq9YgRIXsgEKRLXG8bAC2mM5wChSJddYq2dSbvpS89k";
+import {
+  GET_BLOGPOST_START,
+  GET_BLOGPOST_SUCCESS,
+  GET_BLOGPOST_ERROR
+} from "./types";
 
 export const getBlogPost = () => dispatch => {
   dispatch({ type: GET_BLOGPOST_START });
