6f387ff514c61ae8309daa3860d248854e527dde
danielkyman <danny.kyman@gmail.com>
2020-04-13 11:06:13 -0700

    implemented CRUD operations using knex

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -7,9 +7,9 @@ const Intro = () => {
       <div className="inner-home">
         <p>
           I am a full stack web developer and software engineer specializing in
-          JavaScript, Node.js and much more. I developed this platform to share
-          my knowledge with the hopes of benefiting those starting a journey in
-          the world of web development. I hope you enjoy!
+          JavaScript, Node.js, MySQL and much more. I developed this platform to
+          share my knowledge with the hopes of benefiting those starting a
+          journey in the world of web development. I hope you enjoy!
         </p>
       </div>
     </div>
