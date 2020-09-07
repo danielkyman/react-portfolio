6929d20d5a314d12223f2311e54a1abe7a12fa09
danielkyman <danny.kyman@gmail.com>
2020-09-07 16:44:37 -0700

    fixed typo on sample about page and corrected contact page margin

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -6,7 +6,24 @@ const AboutMe = () => {
       <h1>About Me</h1>
       <div className="inner-home">
         <p>
-          Ever since I built my first computer when I was 12 years old I have been obsessed with hardware and software development.  I began developing web applications shortly after that, using the limited self-study resources available online in the early 2000's.  Throughout that time I became extensively knowledgeable not only with the development of web based applications but with graphic design using Photoshop and Illustrator, 3d modeling and CAD using a variety of the most popular design suits, electrical engineering and electromagnetism concepts while designing and manufacturing my own printed circuit boards and winding my own transformers, and embedded bare metal software design including writing my own firmware in C , C++ and arduino.  After years of self study, I decided to further my education specifically in regards to web development and enroll at Lambda, a 9 month full stack coding boot camp.  While studying at Lambda I came up with the idea to develop my own online platform to share some of the knowledge I have picked up over the years in the form of articles that I write, open source projects that I have worked on, and articles written by others that I have found useful.
+          Ever since I built my first computer when I was 12 years old I have
+          been obsessed with hardware and software development. I began
+          developing web applications shortly after that, using the limited
+          self-study resources available online in the early 2000's. Throughout
+          that time I became extensively knowledgeable not only with the
+          development of web based applications but with graphic design using
+          Photoshop and Illustrator, 3d modeling and CAD using a variety of the
+          most popular design suits, electrical engineering and electromagnetism
+          concepts while designing and manufacturing my own printed circuit
+          boards, winding my own transformers, and embedded bare metal software
+          design including writing my own firmware in C , C++ and arduino. After
+          years of self study, I decided to further my education specifically in
+          regards to web development and enroll at Lambda, a 9 month full stack
+          coding boot camp. While studying at Lambda I came up with the idea to
+          develop my own online platform to share some of the knowledge I have
+          picked up over the years in the form of articles that I write, open
+          source projects that I have worked on, and articles written by others
+          that I have found useful.
         </p>
         <h2>Experience</h2>
         <h3>Front End Development</h3>
@@ -27,33 +44,18 @@ const AboutMe = () => {
         </ul>
         <h3>Back End Development</h3>
         <ul>
-          <li>
-            RESTful Web APIs with Express and Node.js
-          </li>
-          <li>
-            Server-side Routing,Express Middleware
-          </li>
-          <li>
-            Relational Databases and SQL
-          </li>
-          <li>
-            Querying Data, Migrations and Seeding
-          </li>
-          <li>
-            Data Modeling
-          </li>
-          <li>
-            Authentication
-          </li>
-          <li>
-            Sessions and Cookies
-          </li>
+          <li>RESTful Web APIs with Express and Node.js</li>
+          <li>Server-side Routing,Express Middleware</li>
+          <li>Relational Databases and SQL</li>
+          <li>Querying Data, Migrations and Seeding</li>
+          <li>Data Modeling</li>
+          <li>Authentication</li>
+          <li>Sessions and Cookies</li>
           <li>JSON Web Tokens</li>
           <li>Client Side Authentication</li>
           <li>Automated Testing</li>
           <li>Testing React Applications</li>
           <li>Testing Web APIs</li>
-
         </ul>
       </div>
     </div>
