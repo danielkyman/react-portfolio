3d6ca87d02760834445b70988e919e27f0edbbf2
Daniel Kyman <danny.kyman@gmail.com>
2020-09-07 17:17:35 -0700

    Updates

---
+++ precache-manifest.cd5b9b339647422923821980f191c487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d903ada3d97b9d0ac2567cdc2de82c9",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9f1abf4ea3d3e8d6ff2c",
+    "url": "/react-portfolio/static/css/main.5bb6b5d7.chunk.css"
+  },
+  {
+    "revision": "3ed6709cb2255ed89858",
+    "url": "/react-portfolio/static/js/2.4c0bb969.chunk.js"
+  },
+  {
+    "revision": "e3581f43e9607d08a6af06de2ed5c4f1",
+    "url": "/react-portfolio/static/js/2.4c0bb969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f1abf4ea3d3e8d6ff2c",
+    "url": "/react-portfolio/static/js/main.54eab6ea.chunk.js"
+  },
+  {
+    "revision": "0346f5e54be923396aed",
+    "url": "/react-portfolio/static/js/runtime-main.8a7d631b.js"
+  }
+]);
